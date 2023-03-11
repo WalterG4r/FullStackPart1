2fa42e8118bb76694d2c63a1b1fd47eac63052cd
Walter Mario Gardella Sambeth <w.m.gardella.sambeth@gmail.com>
2023-03-11 16:36:46 -0300

    Part 1 excercise 1.3

---
+++ courseinfo/1.3/src/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.p1} />
+      <Part part={props.p2} />
+      <Part part={props.p3} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.ex1.exercises + props.ex2.exercises + props.ex3.exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = { name: "Fundamentals of React", exercises: 10 };
+  const part2 = { name: "Using props to pass data", exercises: 7 };
+  const part3 = { name: "State of a component", exercises: 14 };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content p1={part1} p2={part2} p3={part3} />
+      <Total ex1={part1} ex2={part2} ex3={part3} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
