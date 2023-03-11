3d29295ab455a2493aca1fd69aabd927017f7372
Walter Mario Gardella Sambeth <w.m.gardella.sambeth@gmail.com>
2023-03-11 16:24:15 -0300

    Part 1 excercise 1.2

---
+++ courseinfo/1.2/src/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.p1} exercises={props.ex1} />
+      <Part part={props.p2} exercises={props.ex2} />
+      <Part part={props.p3} exercises={props.ex3} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        p1={part1}
+        p2={part2}
+        p3={part3}
+        ex1={exercises1}
+        ex2={exercises2}
+        ex3={exercises3}
+      />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
